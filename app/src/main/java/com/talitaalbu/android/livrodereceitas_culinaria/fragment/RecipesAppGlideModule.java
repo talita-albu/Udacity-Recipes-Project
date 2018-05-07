eb34de842531f3f5c2464fa48c2cfb4ed1e804a4
talita-albu <talita.albu3@gmail.com>
2018-05-07 18:05:45 -0300

    Adição de Widget para o aplicativo

--- app/src/main/java/com/talitaalbu/android/livrodereceitas_culinaria/fragment/RecipesAppGlideModule.java
+++
@@ -1,8 +0,0 @@
-package com.talitaalbu.android.livrodereceitas_culinaria.fragment;
-
-import com.bumptech.glide.annotation.GlideModule;
-import com.bumptech.glide.module.AppGlideModule;
-
-@GlideModule
-public final class RecipesAppGlideModule extends AppGlideModule {
-}
